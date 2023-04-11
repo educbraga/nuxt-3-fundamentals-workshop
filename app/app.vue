5f4b9b21fd0f56e4d344fb40866f79de35db78b2
Eduardo Braga <eduardocbraga@hotmail.com>
2023-04-11 14:24:55 -0300

    refatora página principal

--- app/app.vue
+++ app/app.vue
@@ -1,53 +1,24 @@
 <script setup>
 import { computed, ref } from "vue";
-
-const photoGallery = ref([]);
-
-const numberOfPhotos = computed(() => {
-  return photoGallery.value.length;
-});
-
-const evenAlbums = computed(() => {
-  return photoGallery.value.filter((item) => item.albumId % 2 === 0);
-});
-
-const oddAlbums = computed(() => {
-  return photoGallery.value.filter((item) => item.albumId % 2 !== 0);
-});
-
-const albumPercentage = computed(() => {
-  return evenAlbums.value.length / numberOfPhotos.value;
-});
-
-function fetchPhotoGallery() {
-  fetch("https://jsonplaceholder.typicode.com/photos")
-    .then((response) => response.json())
-    .then((json) => {
-      photoGallery.value = json;
-    });
-}
+import TodoViewer from "./components/TodoViewer";
+import PhotoGallery from "./components/PhotoGallery";
 </script>
 <template>
   <div class="container">
     <div class="section">
-      <h1 class="title">Photo Gallery</h1>
-      <button @click="fetchPhotoGallery">Fetch Photo Gallery</button>
-      <p>
-        {{ numberOfPhotos }} photos ({{ evenAlbums.length }} even albums
-        {{ oddAlbums.length }} odd albums)
-      </p>
-      <ul class="photo-gallery-grid">
-        <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
-          <img :src="photo.thumbnailUrl" />
-        </li>
-      </ul>
+      <div class="columns">
+        <div class="column">
+          <PhotoGallery />
+        </div>
+        <div class="column">
+          <TodoViewer title="Test"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
 <style lang="scss">
-@import './node_modules/bulma/bulma.sass';
-.photo-gallery-grid {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-}
+@import "./node_modules/bulma/bulma.sass";
+@import "./assets/styles/main.scss";
 </style>
