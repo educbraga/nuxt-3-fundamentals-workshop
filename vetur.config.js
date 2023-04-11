f9684a519a84528f5a576367c218449615eb2c86
Eduardo Braga <eduardocbraga@hotmail.com>
2023-04-10 22:13:02 -0300

    adiciona configuração para o vetur

---
+++ vetur.config.js
@@ -0,0 +1,13 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  projects: [
+    './', // Shorthand for specifying only the project root location
+    {
+      // **required**
+      // Where is your project?
+      // It is relative to `vetur.config.js`.
+      root: './app',
+    }
+  ]
+}
