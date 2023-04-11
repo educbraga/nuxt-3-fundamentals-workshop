58f081ed10a567a52f26fd5a130e01e2762f0963
Eduardo Braga <eduardocbraga@hotmail.com>
2023-04-10 21:11:08 -0300

    refatora lista de tarefas usando a composition api

---
+++ app/app.vue
@@ -0,0 +1,31 @@
+<script>
+  import { computed, ref } from 'vue';
+
+  const todoList = ref([])
+
+  const completedItems = computed(() => {
+      return todoList.value.filter(item => item.completed)
+    })
+  const remainingItems = computed(() => {
+      return todoList.value.filter(item => !item.completed)
+  })
+
+  function fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => response.json())
+      .then(json => {todoList.value = json})
+    }
+
+</script>
+<template>
+  <div>
+    <img src="/todo.jpg" alt="Todo photo"/>
+    <h1>Hello world! :)</h1>
+    <button @click="fetchTodoList">Fetch Data</button>
+    <p>{{completedItems.length}} completed | {{remainingItems.length}} remaining</p>
+    <ul v-for="todo in todoList" :key="`todo-id-${todo.userId}`">
+      <input type="checkbox" :checked="todo.completed"/>
+      <li>{{todo}}</li>
+    </ul>
+  </div>
+</template>
